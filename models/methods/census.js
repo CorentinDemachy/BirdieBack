44215d252390c49d989f562e66f1f46ae520a3b0
Corentin Demachy <corentin.demachy@supelec.fr>
2018-11-17 00:33:57 +0100

    smoothed result format and ordered it by count DESC

--- models/methods/census.js
+++ models/methods/census.js
@@ -5,11 +5,22 @@ const { census } = require('../constructors/index');
 census.groupAndCountCensus = attribute => new Promise(((resolve) => {
   const filter = {
     where: { },
-    attributes: ['age', `${attribute}`, sequelize.fn('count', sequelize.col(`${attribute}`))],
+    attributes: [`${attribute}`, sequelize.fn('count', sequelize.col(`${attribute}`)), [sequelize.fn('AVG', sequelize.col('age')), 'ageAvg']],
     group: [`${attribute}`],
   };
 
-  resolve(census.findAndCountAll(filter));
-}));
+  census.findAndCountAll(filter).then((datas) => {
+    const datarows = datas.rows.map(data => data.get({
+      plain: true,
+    }));
+
+    for (let i = 0; i < datas.count.length; i += 1) {
+      datarows[i].count = datas.count[i].count;
+    }
+    const sortFunction = (a, b) => b.count - a.count;
+    const orderedData = datarows.sort(sortFunction);
+    resolve(orderedData);
+  });
+})).catch(err => Promise.reject(err));
 
 module.exports = census;
