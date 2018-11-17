19afab0b049413f5ef2931d4c46e562f79e0fdc9
Corentin Demachy <corentin.demachy@supelec.fr>
2018-11-17 01:00:07 +0100

    Added order within SQL request, used findAll instead of findAndCountAll

--- models/methods/census.js
+++ models/methods/census.js
@@ -5,21 +5,13 @@ const { census } = require('../constructors/index');
 census.groupAndCountCensus = attribute => new Promise(((resolve) => {
   const filter = {
     where: { },
-    attributes: [`${attribute}`, sequelize.fn('count', sequelize.col(`${attribute}`)), [sequelize.fn('AVG', sequelize.col('age')), 'ageAvg']],
+    attributes: [`${attribute}`, [sequelize.fn('count', sequelize.col(`${attribute}`)), 'count'], [sequelize.fn('AVG', sequelize.col('age')), 'ageAvg']],
     group: [`${attribute}`],
+    order: [[sequelize.fn('count', sequelize.col(`${attribute}`)), 'DESC']],
   };
 
-  census.findAndCountAll(filter).then((datas) => {
-    const datarows = datas.rows.map(data => data.get({
-      plain: true,
-    }));
-
-    for (let i = 0; i < datas.count.length; i += 1) {
-      datarows[i].count = datas.count[i].count;
-    }
-    const sortFunction = (a, b) => b.count - a.count;
-    const orderedData = datarows.sort(sortFunction);
-    resolve(orderedData);
+  census.findAll(filter).then((datas) => {
+    resolve(datas);
   });
 })).catch(err => Promise.reject(err));
 
